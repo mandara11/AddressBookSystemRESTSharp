72b64046dee15a66068fe7639e69d0c1c38e9511
mandara11 <mandaram2345@gmail.com>
2022-09-08 15:39:44 +0530

    Added:UC22

---
+++ UnitTest1.cs
@@ -0,0 +1,61 @@
+using Newtonsoft.Json;
+using RestSharp;
+using System.Net;
+
+namespace AddressBookSystemRESTSharp
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Email { get; set; }
+    }
+    [TestClass]
+    public class UnitTest1
+    {
+        RestClient client;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            //Initialize the base URL to execute requests made by the instance
+            client = new RestClient("http://localhost:4000");
+        }
+        private IRestResponse GetContactList()
+        {
+            //Arrange
+            //Initialize the request object with proper method and URL
+            RestRequest request = new RestRequest("/Contacts", Method.GET);
+            //Act
+            // Execute the request
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+        /* UC22:- Ability to Read Entries of Address Book from JSONServer.
+                  - Use RESTSharp for REST Api Calls from MSTest Test Code.
+                  - Update Address Book Memory of the Data retrieved from JSON Server.
+        */
+        [TestMethod]
+        public void ReadEntriesFromJsonServer()
+        {
+            IRestResponse response = GetContactList();
+            // Check if the status code of response equals the default code for the method requested
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            // Convert the response object to list of employees
+            List<Contact> employeeList = JsonConvert.DeserializeObject<List<Contact>>(response.Content);
+            Assert.AreEqual(2, employeeList.Count);
+            foreach (Contact c in employeeList)
+            {
+                Console.WriteLine($"Id: {c.Id}\tFullName: {c.FirstName} {c.LastName}\tPhoneNo: {c.PhoneNumber}\tAddress: {c.Address}\tCity: {c.City}\tState: {c.State}\tZip: {c.Zip}\tEmail: {c.Email}");
+            }
+        }
+
+    }
+}
